b2fecaf199c321726d1a7fbb8b007fa80bbe1f47
art_ymst <captblue.edits@gmail.com>
2025-11-15 09:09:15 +0100

    Update __init__.py

--- physicsgraphpy/__init__.py
+++ physicsgraphpy/__init__.py
@@ -1,5 +1,9 @@
-from .motion import MotionGraph
-from .plot import plot_all
-from .utils import convert_units, format_markdown
+from .motion import MotionGraph, ProjectileMotion
+from .plot import plot_all, plot_projectile, animate_motion
+from .utils import convert_units, format_markdown, export_csv, check_si_units
 
-__all__ = ["MotionGraph", "plot_all", "convert_units", "format_markdown"]
+__all__ = [
+    "MotionGraph", "ProjectileMotion",
+    "plot_all", "plot_projectile", "animate_motion",
+    "convert_units", "format_markdown", "export_csv", "check_si_units"
+]
